ed8e586a8eff93186e956a81af3b7a788d58fb99
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2025-05-04 22:25:56 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -46,7 +46,24 @@ app.post('/login',(req,res)=>{
         })
     })
 });
-
+//ui videos
+app.get('/uivideos',(req,res)=>{
+    mongoClient.connect(conStr).then((clientObject)=>{
+        const db=clientObject.db('video-library');
+        db.collection('ui-videos').find({}).toArray().then(videos=>{
+            res.send(videos)
+        })
+    })
+});
+//ui videos
+app.get('/javavideos',(req,res)=>{
+    mongoClient.connect(conStr).then((clientObject)=>{
+        const db=clientObject.db('video-library');
+        db.collection('java-videos').find({}).toArray().then(videos=>{
+            res.send(videos)
+        })
+    })
+});
 //....................server at port 6060....................
 app.listen(port);
 console.log('http://localhost:'+port);
